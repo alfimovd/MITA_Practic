4382e58608663fccf67644efc73cafe0334444b1
Dima <alfimovd@gmail.com>
2015-02-24 19:42:29 +0700

    Debug and Extension

---
+++ DebugApp/DebugApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DebugApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int count = str.ToInt();
+            var rand = new Random();
+            var dateList = new List<DateTime>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var r = rand.Next();
+                dateList.Add(DateTime.Now.AddMinutes(r));
+            }
+
+        }
+    }
+}
---
+++ DebugApp/DebugApp/StringHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DebugApp
+{
+    public static class StringHelper
+    {
+        // мы расширяем клас string методом ToInt
+        public static int ToInt(this string str)
+        {
+            try
+            {
+                var i = int.Parse(str);
+                return i;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+    }
+}
