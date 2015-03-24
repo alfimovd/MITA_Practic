a5e7068af8794832eac4a20752a7685d016e9118
Dima <alfimovd@gmail.com>
2015-03-24 22:32:07 +0700

    практика 13

---
+++ DatabaseSampel/DatabaseSampel/Db.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseSampel
+{
+    class Db : DbContext
+    {
+        static Db()
+        {
+            System.Data.Entity.Database.SetInitializer(new MigrateDatabaseToLatestVersion<Db, Migrations.Configuration>());   
+        }
+
+        public IDbSet<Item> Items { get; set; }
+        public IDbSet<ChildItem> ChildItems { get; set; } 
+    }
+}
---
+++ DatabaseSampel/DatabaseSampel/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseSampel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new Db();
+            var item = db.Items.FirstOrDefault();
+
+            if (item == null)
+            {
+                item = new Item
+                {
+                    Name = "Item "+ DateTime.Now
+                };
+                db.Items.Add(item);
+            }
+
+            var child = new ChildItem
+            {
+                Name = "Child " + DateTime.Now,
+                parent = item
+            };
+            db.ChildItems.Add(child);
+            db.SaveChanges();
+            Console.WriteLine("{0} - {1}", item.Id, item.Name);
+
+            var latest = db.ChildItems.Include(ci => ci.parent).Where(ci => ci.Id > 2).ToArray();
+
+            foreach (var it in latest)
+            {
+                Console.WriteLine("{0} - {1}", it.Id, it.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
